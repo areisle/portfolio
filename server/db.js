e3caf6c75d5ff6d09d562e86a0623bdf0e07c15b
abbey reisle <areisle@gmail.com>
2017-11-01 23:49:48 -0700

    added reject case to db

--- server/db.js
+++ server/db.js
@@ -13,7 +13,11 @@ let db = {};
 db.getProjectOutlines = () => {
   return new Promise(function (resolve, reject) {
     connection.query('SELECT name, slug, category, tags FROM Projects', function (err, results) {
-      resolve(results);
+      if (err) {
+        console.log('db error:', err);
+      } else {
+        resolve(results);
+      }
     });
   });
 };
@@ -22,7 +26,11 @@ db.getProject = (name) => {
   return new Promise(function (resolve, reject) {
     let query = `SELECT * FROM Projects WHERE slug="${name}"`;
     connection.query(query, function (err, results) {
-      resolve(results);
+      if (err) {
+        console.log('db error:', err);
+      } else {
+        resolve(results);
+      }
     });
   });
 };
